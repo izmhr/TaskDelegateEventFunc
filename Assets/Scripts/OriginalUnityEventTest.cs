7f4c2b66a6b3220a3e1b172671807e7c38f1672d
izmhr <atsizumi@gmail.com>
2019-09-22 13:15:07 +0900

    UnityEvent もさらっと。

---
+++ Assets/Scripts/OriginalUnityEventTest.cs
@@ -0,0 +1,58 @@
+﻿// 【Unity】UnityEventの用法と用量
+// https://www.urablog.xyz/entry/2016/09/11/080000
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OriginalUnityEventTest : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEngine.Events.UnityEvent myEvent = new UnityEngine.Events.UnityEvent();
+
+    [System.Serializable]
+    public class MyIntEvent : UnityEngine.Events.UnityEvent<int> { }
+    [SerializeField]
+    private MyIntEvent myIntEvent = new MyIntEvent();
+
+    // [System.Serializable]を使ってEditor上に調整パラメータを出す
+    // https://qiita.com/waken/items/30e087480626e3d8229d
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            myEvent.Invoke();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // エディタ上でイベントを登録した場合、ここで登録した引数は無視される。
+            myIntEvent.Invoke(123);
+        }
+
+        // スクリプトだけでイベントを加え、さらに、実行後自らを削除してみる例
+        // https://answers.unity.com/questions/1492047/unityactionunityevent-remove-listener-from-within.html
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            //SetEventFromCode(() => Debug.Log("Invoked Medthod whidh is added from code"));
+            UnityAction myAction = null;
+            myAction = new UnityAction(() =>
+                {
+                    Debug.Log("Invoked Medthod whidh is added from code");
+                    myEvent.RemoveListener(myAction);
+                });
+            SetEventFromCode(myAction);
+        }
+    }
+
+    public void SetEventFromCode(UnityAction action)
+    {
+        myEvent.AddListener(action);
+    }
+}
---
+++ Assets/Scripts/UnityEvent_ButtonTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityEvent_ButtonTest : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void MyAction()
+    {
+        Debug.Log("On Click");
+    }
+
+    public void MyIntAction(int _in)
+    {
+        Debug.Log(_in * _in);
+    }
+}
